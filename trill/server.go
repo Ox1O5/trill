5b27cd5fc630527d421c0bd4af1e64605fd7adb7
Ox1O5 <mushan65535@outlook.com>
2020-07-13 10:07:35 +0800

    v0.1

--- trill/server.go
+++ trill/server.go
@@ -1,11 +1,12 @@
 package trill
 
 import (
-	"net",
+	"net"
 	"fmt"
+	"time"
 )
 
-type IServer struct {
+type IServer interface {
 	Start()
 	Stop()
 	Server()
@@ -23,7 +24,7 @@ func NewServer(name string)  IServer{
 		name : name,
 		ipVersion : "tcp4",
 		ip : "0.0.0.0",
-		port : "9090",
+		port : 9090,
 	}
 	return s
 }
@@ -31,11 +32,49 @@ func NewServer(name string)  IServer{
 func (s *server) Start() {
 	fmt.Printf("[start] server listenner at ip: %s : %d , is starting\n", s.ip, s.port)
 	go func() {
-		add ,err := net.ResolveTCPAddr(s.ipVersion, fmt.Sprintf("%s:%d", s.ip, s.port))
+		addr ,err := net.ResolveTCPAddr(s.ipVersion, fmt.Sprintf("%s:%d", s.ip, s.port))
 		if err != nil {
 			fmt.Println("resolve tcp addr err: ", err)
 			return
 		}
-		listenner, err := net.Lis
+		listenner, err := net.ListenTCP(s.ipVersion, addr )
+		if err != nil {
+			fmt.Println("listen", addr ,"error",err)
+			return
+		}
+		fmt.Println("start trill ", s.name, " success, listenning at ", addr)
+		for {
+			conn, err := listenner.Accept()
+			if err != nil {
+				fmt.Println("Accept error ", err)
+				continue
+			}
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("receive buffer error ", err)
+						continue
+					}
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("Write back error ", err)
+						continue
+					}
+				}
+			}()
+		}
+	}()
+}
+
+func (s *server) Stop()  {
+	fmt.Println("[stop] server name ", s.name)
+}
+
+func (s *server) Server()  {
+	s.Start()
+	for {
+		time.Sleep(10*time.Second)
 	}
+	
 }
---
+++ trill/server_test.go
@@ -0,0 +1,42 @@
+package trill
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
+
+func ClientTest() {
+	fmt.Println("Client Test ... start")
+	time.Sleep(3*time.Second)
+	conn, err := net.Dial("tcp", "127.0.0.1:9090")
+	if err != nil {
+		fmt.Println("client start error ", err)
+		return
+	}
+	for {
+		_, err := conn.Write([]byte("Hello world"))
+		if err != nil {
+			fmt.Println("write error ", err)
+			return
+		}
+		buf := make([]byte, 512)
+		cnt ,err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read from buffer error ", err)
+			return
+		}
+		fmt.Printf("server call back : %s\n", buf[:cnt])
+		time.Sleep(time.Second)
+	}
+}
+
+func TestServer(t *testing.T)  {
+	s := NewServer("trill 0.1")
+
+	go ClientTest()
+
+	s.Server()
+	
+}
